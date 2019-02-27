67df43817808e99b11c29f6f60f63e82d18dce6a
Vitaly-V <gtopbox@gmail.com>
2019-02-27 11:51:05 +0200

    Init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:vector_math/vector_math.dart' show radians;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
